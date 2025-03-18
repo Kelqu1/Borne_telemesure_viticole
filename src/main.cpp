8d5e53520b62453f2d386aa11b494fbc4ab07e89
Kelqu1 <hugolalevee0@gmail.com>
2025-03-18 11:00:53 +0100

    petite erreur

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,30 @@
 #include <Arduino.h>
-#include <Adafruit_Sensor.h>
-#include <WiFi.h>
-#include <ESPAsyncWebServer.h>
 
-AsyncWebServer server(80);
-//debut code de yanis
-const char *ssid = "BORNE_WIFI";
-const char *password = "12345678";
+#define tension_ref 3.3f //tension de reférence pour l'ESP32
+#define nb_etat_max 4096.0 //nombre d'état numérique possible pour un converstisseur 12 bits
+#define ratio_pont_diviseur 4.343f
 
 void setup() {
-   
-    Serial.begin(9600);
-    WiFi.softAP(ssid, password);
-    Serial.println("Point d'accès WiFi activé !");
-    Serial.print("Adresse IP: ");
-    Serial.println(WiFi.softAPIP());
-    //fin code de yanis
-    //debut code hugo 
-
-    //requete pour tester si L'API est activé
-    server.on("/status", HTTP_GET, [](AsyncWebServerRequest *request){
-        String response = "{\"status\": \"ok\", \"ip\": \"" + WiFi.softAPIP().toString() + "\"}";
-        request->send(200, "application/json", response); 
-    });
-     
-    //requete pour avoir la température
-    server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
-        float temperature = 25.60; 
-        String response = "{\"temperature\": " + String(temperature, 2) + "}";
-        request->send(200, "application/json", response);
-    });
-
-    //démarage du serveur
-    server.begin();
-
-    //mise en forme des informations utile dans le terminal
-    Serial.println("status de l'api : /status");
-    Serial.println("temperature     : /temperature");
-    Serial.println("Serveur Web au port 200");
+    Serial.begin(9600);  // Initialisation de la comunication série 
 }
+void loop() {
+    //caluls des tension
+    int valeur_brute=  analogRead(34); // Lecture de la valeur analogique
+    float Tension_GPIO = (valeur_brute / nb_etat_max) * tension_ref; 
+    float Tension_Batterie=Tension_GPIO* ratio_pont_diviseur; 
 
-void loop () 
-{
+    // Affichage des valeurs dans le moniteur série
+    Serial.print("Valeur brute: ");
+    Serial.println( valeur_brute); 
 
+    Serial.print("Tension à l'entrée de l'ESP32: ");
+    Serial.print(Tension_GPIO);  
+    Serial.println(" V");
+
+    Serial.print("Tension de la batterie: ");
+    Serial.print(Tension_Batterie); 
+    Serial.println(" V");
+
+    Serial.println("-------------------------------------");
+    delay(400);
 }
