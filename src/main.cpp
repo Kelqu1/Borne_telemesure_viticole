57c4b290e55146c3273d0ad8a1b6a4b39db9206f
Kelqu1 <hugolalevee0@gmail.com>
2025-03-25 16:11:28 +0100

    fussion DHT22 + capteur batterie

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,24 @@
 //et debut de la phase de developpement
 
 #include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+
+// Définition du capteur
+#define DHTPIN 19        // Broche où est connecté le DHT22
+#define DHTTYPE DHT22   // Type de capteur
 #define tension_ref 3.3f //tension de reférence pour l'ESP32
 #define nb_etat_max 4096.0 //nombre d'état numérique possible pour un converstisseur 12 bits
 #define ratio_pont_diviseur 4.343f
 
+DHT dht(DHTPIN, DHTTYPE);
+
 void setup() {
-    Serial.begin(9600);  // Initialisation de la comunication série 
+    Serial.begin(9600);  // Initialisation de la communication série
+    Serial.println("Initialisation du capteur DHT22...");
+    dht.begin();
 }
+
 void loop() {
     //caluls des tension
     int valeur_brute=  analogRead(34); // Lecture de la valeur analogique
@@ -28,5 +39,23 @@ void loop() {
     Serial.println(" V");
 
     Serial.println("-------------------------------------");
-    delay(400);
+
+    float temperature = dht.readTemperature(); // Lecture de la température en °C
+    float humidite = dht.readHumidity(); // Lecture de l'humidité en %
+
+    if (isnan(temperature) || isnan(humidite)) {
+        Serial.println("Échec de lecture du DHT22 !");// affichage d'une erreur de lecture
+        return;
+    }
+
+    Serial.print("Température: ");
+    Serial.print(temperature);//écriture de la température
+    Serial.println(" °C");
+
+    Serial.print("Humidité: ");
+    Serial.print(humidite); //écriture de l'humidité
+    Serial.println(" %");
+
+    Serial.println("--------------------");
+    delay(2000);  // Attente avant la prochaine lecture
 }
