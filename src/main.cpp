40183c26e59be5efec29e9c7b109df1dbc5afc60
Kelqu1 <hugolalevee0@gmail.com>
2025-03-31 15:56:20 +0200

    simplification et clarification du code+ lecture tension borne retiré

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,43 @@
-//fin de la phase de prototypage 
-//et debut de la phase de developpement
-
 #include <Arduino.h>
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 
-// Définition du capteur
-#define DHTPIN 19        // Broche où est connecté le DHT22
+// CAPTEUR HUMI+TEMP parametres
+#define DHTPIN 19        // Broche où est branché le DHT22
 #define DHTTYPE DHT22   // Type de capteur
+
+// CAPTEUR NIVEAU TENSION
 #define tension_ref 3.3f //tension de reférence pour l'ESP32
-#define nb_etat_max 4096.0 //nombre d'état numérique possible pour un converstisseur 12 bits
-#define ratio_pont_diviseur 4.343f
+#define nb_etat_max 4096.0f //nombre d'état numérique possible pour un converstisseur 12 bits
+#define tensionPin 34 // Broche où est conecté le pont diviseur
 
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
     Serial.begin(9600);  // Initialisation de la communication série
-    Serial.println("Initialisation du capteur DHT22...");
     dht.begin();
+    Serial.println("Initialisation de la borne de télémesure");
 }
 
 void loop() {
-    //caluls des tension
-    int valeur_brute=  analogRead(34); // Lecture de la valeur analogique
-    float Tension_GPIO = (valeur_brute / nb_etat_max) * tension_ref; 
-    float Tension_Batterie=Tension_GPIO* ratio_pont_diviseur; 
-
-    // Affichage des valeurs dans le moniteur série
-    Serial.print("Valeur brute: ");
-    Serial.println( valeur_brute); 
-
-    Serial.print("Tension à l'entrée de l'ESP32: ");
-    Serial.print(Tension_GPIO);  
-    Serial.println(" V");
-
-    Serial.print("Tension de la batterie: ");
-    Serial.print(Tension_Batterie); 
-    Serial.println(" V");
-
-    Serial.println("-------------------------------------");
-
+    //CAPTEUR NIVEAU BATTERIE
+    int valeur_brute=  analogRead(tensionPin); // Lecture de la valeur analogique
+    float Tension_GPIO = (valeur_brute / nb_etat_max) * tension_ref +0.3 ;// cacul tension avec une  
+     
+    //CAPTEUR HUMIDITé ET TEMPERATURE
     float temperature = dht.readTemperature(); // Lecture de la température en °C
     float humidite = dht.readHumidity(); // Lecture de l'humidité en %
 
+    //affichage d'une erreur pour le DHT au besoin 
     if (isnan(temperature) || isnan(humidite)) {
-        Serial.println("Échec de lecture du DHT22 !");// affichage d'une erreur de lecture
+        Serial.println("Échec de lecture du DHT22 !");
         return;
     }
+
+    // Affichage des valeurs dans le moniteur série
+    Serial.print("Tension entrée ESP32: ");
+    Serial.print(Tension_GPIO); //ecriture tension
+    Serial.println(" V");
 
     Serial.print("Température: ");
     Serial.print(temperature);//écriture de la température
@@ -56,6 +47,6 @@ void loop() {
     Serial.print(humidite); //écriture de l'humidité
     Serial.println(" %");
 
-    Serial.println("--------------------");
-    delay(2000);  // Attente avant la prochaine lecture
+    Serial.println("-----------------------------");
+    delay(1000);  // Attente avant les prochaines lectures
 }
